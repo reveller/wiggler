19270425e383992a422739a8a49855609aaca656
AZNova <steven.feltner@gmail.com>
2014-01-17 21:03:16 -0400

    Initial commit.

---
+++ wiggler2/src/wiggler2/CmdLine.java
@@ -0,0 +1,111 @@
+package wiggler2;
+
+
+//
+//[1:29:39 PM] Wade Hought: load it with:
+//[1:29:44 PM] Wade Hought: CmdLine.load( args );
+//[1:29:50 PM] Wade Hought: where args is what comes into main( ... )
+//[1:30:02 PM] Wade Hought: then you can do things like
+//[1:30:10 PM] Wade Hought: if ( CmdLine.get( "debug" ) != null ) {
+//[1:30:34 PM] Wade Hought: or
+//[1:30:37 PM] Steven Feltner: this is your code?
+//[1:30:38 PM] Wade Hought: String passphrase = CmdLine.get( "passphrase" );
+//[1:30:42 PM] Wade Hought: yes, my code
+//[1:31:06 PM] Wade Hought: it basically just parses the cmdline into a bunch 
+//             of name=value pairs
+//[1:31:12 PM] Wade Hought: and dumps them into a hashmap
+//[1:31:46 PM] Wade Hought: then does some singleton/static stuff so the values 
+//             are simple to pull from anywhere in the code - vs having to pass 
+//             args all over creation to have access to them
+
+import java.util.*;
+
+
+public class CmdLine {
+	private HashMap	argMap;
+
+	//
+	// singleton this object... main reason for this is so that a main object can
+	// set it up, but then not have to pass it around to other methods that may 
+	// need to access cmdline args.  Simple static methods to access, done.
+	private static CmdLine	singleton;	
+	private CmdLine() {		// no external instantiations of the object allowed, just internal
+		argMap = new HashMap();
+	}	
+	public Object clone() throws CloneNotSupportedException {		// don't let anyone clone the object
+		throw new CloneNotSupportedException();
+	}
+	private static synchronized CmdLine init() {	// create/get the singleton
+		if ( singleton == null ) {
+			singleton = new CmdLine();
+		}
+		return singleton;
+	}
+	
+	// load an array of arguments of the form:  -paramname:value   into the hashmap for retrieval
+	public static void load( String[] args ) throws java.lang.IllegalArgumentException {
+		CmdLine	cl = CmdLine.init();
+		
+		String	arg, key, value;
+		int		offset, filecount = 0;
+
+		for ( int i=0 ; i<args.length ; i++ ) {
+			arg = args[i];
+			if ( arg.indexOf( "?" ) >= 0 ) {
+				throw new IllegalArgumentException( "Displaying help" );
+			}
+			else if ( arg.substring( 0, 1 ).equals( "-" ) ) {
+				offset = arg.indexOf( "=" );
+				if ( offset == -1 ) {
+					offset = arg.indexOf( ":" );
+				}
+
+				if ( offset >= 1 ) {
+					// this is an option of the type "-option:value" or "-option=value"
+					key = arg.substring( 0, offset ).substring( 1 );	// remove the '-' too
+					value = arg.substring( offset+1 );
+
+					cl.argMap.put( key, value );
+				}
+				else {
+					// this is a plain flag-type option, "-debug"
+					key = arg.substring( 1 );
+
+					cl.argMap.put( key, "true" );
+				}
+			}
+			else {
+				// this is a standalone option without a '-', treat it as a filename and
+				// store it in the map with a special key
+				cl.argMap.put( "filename" + String.valueOf( filecount ), arg );
+				filecount++;
+			}
+
+		}
+	}
+
+	public static void setDefaultValue( String name, String defaultValue ) {
+		CmdLine	cl = CmdLine.init();
+
+		if ( ! cl.argMap.containsKey( name ) ) {
+			cl.argMap.put( name, defaultValue );
+		}
+	}
+	
+	public static Set keySet() {
+		return CmdLine.init().argMap.keySet();
+	}
+	public static String get( String key ) {
+		return (String) CmdLine.init().argMap.get( key );
+	}
+	public static String getString( String key ) {
+		return (String) CmdLine.init().argMap.get( key );
+	}
+	public static Integer getInteger( String key ) {
+		return (Integer) CmdLine.init().argMap.get( key );
+	}
+	public static void put( String key, String value ) {
+		CmdLine.init().argMap.put( key, value );
+	}
+
+}
---
+++ wiggler2/src/wiggler2/Wiggler2.java
@@ -0,0 +1,205 @@
+package wiggler2;
+
+import java.awt.AWTException;
+import java.awt.MouseInfo;
+import java.awt.Robot;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+// import gnu.getopt.GetOpt;
+
+
+
+
+public class Wiggler2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main( String[] args ) {
+		
+		wiggler( args );
+		
+		// foo( String.class );
+		
+		// new CodeTest();
+	}
+	
+	private static void wiggler( String[] args ) {
+
+		System.out.println( "Getting my wiggle on..." );
+		Robot robot;
+		try {
+			robot = new Robot();
+			int		baseX = (int) MouseInfo.getPointerInfo().getLocation().getX();
+			int		baseY = (int) MouseInfo.getPointerInfo().getLocation().getY();
+			int		idleDelay = 60;	// in seconds
+			int		timer = 0;
+			int 	timeToStop = 0;
+			boolean debug = false;
+			
+			CmdLine.load( args );
+			if ( CmdLine.get( "debug" ) != null){
+				debug = true;
+				System.out.println( "debug set to [" + (debug ? "true" : "false") + "]");					
+			}
+			if ( (idleDelay = Integer.parseInt(CmdLine.get( "interval" ))) > 0 ) {
+				if (debug)
+					System.out.println( "interval set to " + idleDelay );					
+			}
+			if ( (timeToStop = Integer.parseInt(CmdLine.get( "stoptime" ))) > 0){
+				if (debug)
+					System.out.println( "stop time set to " + timeToStop );					
+			}
+			
+			
+//			Getopt g = new Getopt("wiggler", args, "i:t:"); //-D to enable debug log
+//			while((opt = g.getopt()) != -1) {
+//			    switch(opt){
+//			        case 'i':								//  interval between wiggles
+//			        	idleDelay = g.getOPtArg();
+//			            break;
+//			        case 't':								// time to stop wiggling
+//			            timeToStop = g.getOptArg();                    
+//			            break;
+//			        case ':':                               /* error - missing operand */
+//			            fprintf(stderr, "Option -%c requires an operand\n", optopt);
+//			            break;
+//			        case '?':                               /* error - unknown option */
+//			            fprintf(stderr,"Unrecognized option: -%c\n", optopt);
+//			            break;
+//			    }
+//			}
+			
+//			if (args.length > 0 ) {
+//				try {
+//					idleDelay = Integer.parseInt( args[0] );
+//				}
+//				catch ( Exception e ) {
+//					System.out.println( "Ignoring idleDelay parameter: " + args[0] + ", use an integer next time" );
+//				}
+//			}
+//			System.out.println( "Using idle delay: " + idleDelay );
+			
+			while ( true ) {
+				robot.delay( 1000 );
+				timer++;	// timer is in seconds
+
+				int curX = (int) MouseInfo.getPointerInfo().getLocation().getX();
+				int curY = (int) MouseInfo.getPointerInfo().getLocation().getY();
+				
+				// movement idle timer
+				if ( timer < idleDelay ) {
+					// if the mouse has moved, update base, reset timer
+					if ( curX != baseX  ||  curY != baseY ) {
+						baseX = curX;
+						baseY = curY;
+						timer = 0;
+						if (debug)
+							System.out.println( "idle timer reset" );
+					}
+					continue;
+				}
+
+				if (debug)
+					System.out.println( "wiggling" );
+				
+				// beyond the timer, wiggle
+				int	endX = baseX + (int) (15f * (Math.random()-.5f));
+				int	endY = baseY + (int) (15f * (Math.random()-.5f));
+				
+				for ( int i=0 ; i<100 ; i++ ){  
+				    int x = ((endX * i)/100) + (baseX*(100-i)/100);
+				    int y = ((endY * i)/100) + (baseY*(100-i)/100);
+				    robot.mouseMove( x, y );
+				    robot.delay( 5 );
+				}				
+			    
+			    // then move back to the origin
+			    robot.mouseMove( baseX, baseY );
+			    
+			    // reset the timer to 70% and let the idle loop continue
+			    timer = (int) ((float)idleDelay * .7f);
+			}
+		} 
+		catch ( Exception e ) {
+			System.out.println( "Wiggler: " + e.toString() );
+		}
+		
+	}
+
+	/*
+	public static void foo( Class ... cl ) {
+		System.out.println( cl );
+		String	bar = "wade";
+		for ( Class c : cl ) {
+			// if ( bar instanceof cl )
+			System.out.println( c );
+			System.out.println( bar.getClass() );
+			if ( c.isAssignableFrom( bar.getClass() ) )
+				System.out.println( "woo" );
+		}
+	}
+
+	public CodeTest() {
+		try {
+			BlockingQueue	queue = new LinkedBlockingQueue();
+			
+			LinkedBlockingQueue	queue2 = new LinkedBlockingQueue();
+
+			// create each thread
+			Thread	t1, t2;
+			t1 = new Consumer( queue );
+			t2 = new Consumer( queue );
+			
+			for ( int i=0 ; i<50000 ; i++ ) {
+				queue.offer( (int) (Math.random() * 1000) );
+			}
+			queue.offer( -1 );
+			queue.offer( -1 );
+
+			long start = System.nanoTime();
+			t1.start();
+			t2.start();
+			while ( t1.isAlive() && t2.isAlive() ) Thread.sleep( 25 );
+			System.out.println( "Run nanos: " + (System.nanoTime() - start) );					
+		}
+		catch ( Exception e ) {
+			e.printStackTrace();
+		}
+	}	
+
+	private class Consumer extends Thread {
+		BlockingQueue	queue;
+		
+		public Consumer( BlockingQueue bq ) {
+			queue = bq;
+		}
+		
+		@Override
+		public void run() {
+			long	threadId = Thread.currentThread().getId();
+			
+			while ( true ) {
+				Integer i, last = 0, timeout = 1;
+				try {
+					i = (Integer) queue.take();
+					if ( i == -1 ) return;
+					
+					if ( !(i % 2 == threadId % 2) ) {
+						if ( i == last )
+							timeout += 2;
+						else
+							timeout = 1;
+						
+						queue.offer( i );
+						Thread.sleep( timeout );
+						
+						// System.out.println( "Thread: " + threadId + " got " + i );						
+					}
+				}
+				catch ( InterruptedException e ) {}				
+			}
+		}
+	}
+	*/
+}
