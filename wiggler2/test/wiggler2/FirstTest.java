63977298fec9598817fe4ff0c1ad43c22a811cdd
Steven Feltner <sfeltner@godaddy.com>
2014-01-17 22:00:30 -0700

    Added FirstTest as a JUnit4 test case from Linux

---
+++ wiggler2/test/wiggler2/FirstTest.java
@@ -0,0 +1,15 @@
+package wiggler2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FirstTest {
+
+    @Test
+    public void test() {
+      //  fail("Not yet implemented");
+      assertEquals(2+2, 4);
+    }
+
+}
